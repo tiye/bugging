40b89c4a2493ee2dd7ca0ac537bb012088adf9c1
jiyinyiyong <jiyinyiyong@gmail.com>
2013-12-17 02:10:41 +0800

    coping code

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+    "os/exec"   
+    "os"
+    "fmt"
+    "io"
+    "log"
+    "github.com/howeyc/fsnotify"
+    "time"
+)
+
+func main() {
+    runTest()
+    watcher, err := fsnotify.NewWatcher()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    done := make(chan bool)
+    timestamp := int64(0)
+
+    // Process events
+    go func() {
+        for {
+            select {
+            case ev := <- watcher.Event:
+                log.Println("event:", ev)
+
+                // runTest()
+                newTime := time.Now().Unix()
+                diffTime := newTime - timestamp
+                if diffTime > 1 {
+                    go runTest()
+                }
+                timestamp = newTime
+            case err := <-watcher.Error:
+                log.Println("error:", err)
+            }
+        }
+    }()
+
+    err = watcher.Watch("./")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    <-done
+
+    /* ... do stuff ... */
+    watcher.Close()
+}
+
+func runTest() {
+    time.Sleep(1 * time.Second)
+    fmt.Println()
+    fmt.Println()
+    fmt.Println()
+    fmt.Println("====== bugging ======")
+    fmt.Println()
+    fmt.Println()
+    fmt.Println()
+    cmd := exec.Command("go", "test")
+    stdout, err := cmd.StdoutPipe()
+    if err != nil {
+        fmt.Println(err)
+    }
+    stderr, err := cmd.StderrPipe()
+    if err != nil {
+        fmt.Println(err)
+    }
+    err = cmd.Start()
+    if err != nil {
+        fmt.Println(err)
+    }
+    go io.Copy(os.Stdout, stdout) 
+    go io.Copy(os.Stderr, stderr) 
+    cmd.Wait()
+}
